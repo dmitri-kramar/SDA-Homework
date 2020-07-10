d843ce584c15f7a29106f2ed873d9b1a8ff8484a
Dmitri Kramar <dmitri.kramar@outlook.com>
2020-07-10 21:52:11 +0300

    Made small changes to the adapter exercise.

--- Design Patterns & Good Practices/src/adapter/IOS.java
+++ Design Patterns & Good Practices/src/adapter/IOS.java
@@ -11,10 +11,12 @@ public class IOS {
     public void chargeIOS() throws InterruptedException {
         System.out.println("Charging an IOS phone:");
         System.out.print(chargeLevel + "% ");
+
         for (int i = 0; i <= 10; i++) {
             Thread.sleep(300);
             System.out.print("|");
         }
+
         chargeLevel = 100;
         System.out.println(" " + chargeLevel + "%" + "\n");
     }
--- Design Patterns & Good Practices/src/adapter/WindowsPhone.java
+++ Design Patterns & Good Practices/src/adapter/WindowsPhone.java
@@ -2,20 +2,22 @@ package adapter;
 
 public class WindowsPhone {
 
-    private String chargeLevel;
+    private int chargeLevel;
 
     public WindowsPhone(int chargeLevel) {
-        this.chargeLevel = chargeLevel + "%";
+        this.chargeLevel = chargeLevel;
     }
 
     public void chargeWindowsPhone() throws InterruptedException {
         System.out.println("Charging an WindowsPhone phone:");
         System.out.print(chargeLevel + "% ");
+
         for (int i = 0; i <= 10; i++) {
             Thread.sleep(300);
             System.out.print("|");
         }
-        chargeLevel = "100%";
-        System.out.println(" " + chargeLevel + "\n");
+
+        chargeLevel = 100;
+        System.out.println(" " + chargeLevel + "%" + "\n");
     }
 }
